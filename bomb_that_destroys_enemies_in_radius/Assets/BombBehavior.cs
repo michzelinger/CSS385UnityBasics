a8ecffe2fd0560357e7af28548db3b0dd535e65a
michzelinger <mzelin@uw.edu>
2021-04-09 19:09:39 -0700

    Bomb thingy

---
+++ bomb_that_destroys_enemies_in_radius/Assets/BombBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class BombBehavior : MonoBehaviour
+{
+
+    public void DetonateBomb()
+    {
+        GameObject blast = Instantiate(Resources.Load("Prefabs/BlastRadius") as GameObject);
+        Vector3 p = this.transform.position;
+        blast.transform.position = p;
+        Destroy(this.gameObject);
+    }
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            DetonateBomb();
+        }
+    }
+    
+}
